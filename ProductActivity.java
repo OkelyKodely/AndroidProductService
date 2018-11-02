40d8f1e972a7ae47d3bd4bd5e35e7bf4a8545f61
Daniel Cho <dh.cho428@gmail.com>
2018-11-02 05:31:52 -0700

    Add files via upload

--- ProductActivity.java
+++ ProductActivity.java
@@ -1,6 +1,8 @@
 package service.androidproductservice;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,10 +10,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -26,9 +33,50 @@ public class ProductActivity extends AppCompatActivity {
 
     private String[] fruits = null;
 
-    private ArrayList<String> products = new ArrayList<String>();
+    private ArrayList<Product> products = new ArrayList<Product>();
 
-    private ArrayAdapter<String> arrayAdapter = null;
+    private CustomAdapter adapter = null;
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return products.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.customlayout, null);
+
+            ImageView iv = (ImageView) convertView.findViewById(R.id.imageView);
+            TextView tv1 = (TextView) convertView.findViewById(R.id.product_name);
+            TextView tv2 = (TextView) convertView.findViewById(R.id.product_price);
+
+            Bitmap bmp = BitmapFactory.decodeByteArray(products.get(position).image, 0, products.get(position).image.length);
+
+            iv.setImageBitmap(bmp);
+            tv1.setText(products.get(position).productName);
+            tv2.setText("  $" + products.get(position).price);
+
+            return convertView;
+        }
+    }
+
+    class Product {
+        private byte[] image;
+        private String productName;
+        private String price;
+    }
 
     public Connection getConnection()
     {
@@ -68,7 +116,11 @@ public class ProductActivity extends AppCompatActivity {
             products.clear();
             while(rs.next())
             {
-                products.add(rs.getString("productName"));
+                Product p = new Product();
+                p.image = rs.getBytes("image");
+                p.productName = rs.getString("productName");
+                p.price = rs.getString("price");
+                products.add(p);
             }
             rs.close();
             ps.close();
@@ -87,14 +139,7 @@ public class ProductActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params) {
             getProducts(category);
-
-            // Create a List from String Array elements
-            final List<String> fruits_list = products;
-
-            // Create an ArrayAdapter from List
-            arrayAdapter = new ArrayAdapter<String>
-                    (ProductActivity.this, android.R.layout.simple_list_item_1, fruits_list);
-
+            adapter = new CustomAdapter();
             return "";
         }
 
@@ -119,18 +164,17 @@ public class ProductActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                System.out.println(((TextView)view).getText().toString());
                 Intent myIntent = new Intent(ProductActivity.this, PActivity.class);
-                myIntent.putExtra("productName", ((TextView)view).getText().toString());
+                myIntent.putExtra("productName", ((TextView)view.findViewById(R.id.product_name)).getText().toString());
                 ProductActivity.this.startActivity(myIntent);
             }
         });
         SimpleTask simple = new SimpleTask();
         simple.execute();
         while(true) {
-            if(arrayAdapter == null)
+            if(adapter == null)
                 continue;
-            lv.setAdapter(arrayAdapter);
+            lv.setAdapter(adapter);
             break;
         }
     }
